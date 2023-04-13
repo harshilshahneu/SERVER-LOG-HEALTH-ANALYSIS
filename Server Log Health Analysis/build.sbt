587119c2c715e8bf26510a5bec6875ec0400a93f
Harshil Shah <shah.harshil2@northeastern.edu>
2023-04-13 18:48:53 -0400

    push json string from producer

--- Server Log Health Analysis/build.sbt
+++ Server Log Health Analysis/build.sbt
@@ -9,3 +9,12 @@ lazy val root = (project in file("."))
 
 libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.8.0"
 libraryDependencies += "org.apache.kafka" %% "kafka" % "2.8.0"
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
+libraryDependencies += "org.json4s" %% "json4s-jackson" % "3.6.11"
+libraryDependencies += "org.json4s" %% "json4s-native" % "3.6.11"
+
+libraryDependencies += "io.circe" %% "circe-core" % "0.14.1"
+libraryDependencies += "io.circe" %% "circe-generic" % "0.14.1"
+libraryDependencies += "io.circe" %% "circe-parser" % "0.14.1"
+
+
--- Server Log Health Analysis/src/main/scala/Producer.scala
+++ Server Log Health Analysis/src/main/scala/Producer.scala
@@ -1,22 +1,43 @@
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
-
-import scala.io.Source
 import java.util.Properties
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.json4s.native.Serialization
+import org.json4s.{DefaultFormats, NoTypeHints}
+import scala.io.Source
+import scala.util.parsing.combinator.RegexParsers
 
-object Producer extends App{
+object Producer extends App with RegexParsers {
+  // Define the log format using parser combinators
+  def ipAddress: Parser[String] = """\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}""".r
+
+  def logEntry: Parser[String] = ipAddress
+
+  // Configure Kafka producer properties
   val props = new Properties()
   props.put("bootstrap.servers", "localhost:9092")
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
+  // Create the Kafka producer
   val producer = new KafkaProducer[String, String](props)
 
+  // Set up JSON serialization
+  implicit val formats = DefaultFormats.withHints(NoTypeHints)
+
+  // Read the log file and send each line to Kafka after parsing it as JSON
   val logFile = "src/main/resources/logfiles.log"
   val source = Source.fromFile(logFile)
   for (line <- source.getLines) {
-    val record = new ProducerRecord[String, String]("logs", null, line)
-    producer.send(record)
+    parse(logEntry, line) match {
+      case Success(ipAddress, _) =>
+        val json = Serialization.write(Map("ip" -> ipAddress))
+        val record = new ProducerRecord[String, String]("logs", null, json)
+        producer.send(record)
+      case _ =>
+        println(s"Failed to parse log line")
+      // Ignore lines that don't match the log format
+    }
   }
 
+  source.close()
   producer.close()
 }
