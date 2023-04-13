be44215da1397781609953b0fb956ca9c8357f77
Harshil Shah <shah.harshil2@northeastern.edu>
2023-04-13 19:08:40 -0400

    scala consumer

---
+++ Server Log Health Analysis/src/main/scala/Consumer.scala
@@ -0,0 +1,33 @@
+import java.util.Properties
+import scala.collection.JavaConverters._
+import org.apache.kafka.clients.consumer.{ConsumerRecords, KafkaConsumer}
+import io.circe.parser.decode
+import io.circe.generic.auto._
+
+case class LogRecord(ip: String, timestamp: String, request: String, responseCode: Int)
+
+object Consumer extends App {
+
+  val props = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+  props.put("group.id", "test-consumer-group")
+  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+
+  val consumer = new KafkaConsumer[String, String](props)
+  consumer.subscribe(List("logs").asJava)
+
+  while (true) {
+    val records: ConsumerRecords[String, String] = consumer.poll(100)
+    records.asScala.foreach { record =>
+      val jsonString = record.value()
+      println(s"Raw JSON string: $jsonString")
+      decode[LogRecord](jsonString) match {
+        case Left(error) => println(s"Failed to decode JSON: ${error.getMessage}")
+        case Right(logRecord) => println("log : " + logRecord)
+      }
+    }
+  }
+
+  consumer.close()
+}
