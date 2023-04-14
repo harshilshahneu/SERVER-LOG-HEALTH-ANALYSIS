bd35e41286689a809a81a863f4632ccaaac22d5d
Harshil Shah <shah.harshil2@northeastern.edu>
2023-04-13 21:12:54 -0400

    rest of the attribute except response time

--- Server Log Health Analysis/src/main/scala/Producer.scala
+++ Server Log Health Analysis/src/main/scala/Producer.scala
@@ -1,50 +1,84 @@
-import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.json4s.native.Serialization
 import org.json4s.{DefaultFormats, NoTypeHints}
+
+import java.util.Properties
 import scala.io.Source
 import scala.util.parsing.combinator.RegexParsers
 
 object Producer extends App with RegexParsers {
   // Define the log format using parser combinators
-  def ipAddress: Parser[String] = """\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}""".r
-  def dash: Parser[String] = "-"
-  def dateTime: Parser[String] = """\[\d{2}/\w+/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4}\]""".r
-  def requestType: Parser[String] = """"(POST|GET|PUT|DELETE|HEAD) """.r ^^ {
+  def ipAddress: Parser[String] = """\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}""".r //correct
+  def dash: Parser[String] = "-" //last
+  def dateTime: Parser[String] = """\[(\d{2}/\w+/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})\]""".r ^^ {
+    _.replaceAll("\\[", "").replaceAll("\\]", "")
+  } //correct
+  def request: Parser[String] = """"(POST|GET|PUT|DELETE) """.r ^^ {
     _.replaceAll("\"", "").replaceAll(" ", "")
-  }
+  } //correct
 
-  def logEntry: Parser[(String, String, String)] = ipAddress ~ dash ~ dash ~ dateTime ~ requestType ^^ {
-    case ip ~ _ ~ _ ~ ts ~ rt => (ip, ts, rt)
-  }
+  def endpoint: Parser[String] = """/?[a-zA-Z0-9/_-]*""".r  //correct
 
-  // Configure Kafka producer properties
+  def protocol: Parser[String] = """HTTP/\d\.\d""".r //correct
+
+  def status: Parser[String] = """\s*"\s*(\d{3})""".r ^^ {
+    _.replaceAll("\"", "").trim()
+  } //correct
+  def bytes: Parser[String] = """\d+""".r //correct
+
+  def referrer: Parser[String] =
+    """\s*"(-|http://[a-zA-Z0-9._/-]*)""".r ^^ {
+      _.replaceAll("\"", "").trim()
+    } //correct
+ def userAgent: Parser[String] = """.*""".r ^^ {
+   _.replaceAll("\\\" \\\"", "")
+ }
+
+
+  def logEntry: Parser[(String, String, String, String, String, String, String, String, String)] =
+    ipAddress ~ dash ~ dash ~ dateTime ~ request ~ endpoint ~ protocol ~ status ~ bytes ~ referrer ~ userAgent ^^ {
+      case ip ~ _ ~ _ ~ time ~ req ~ endpoint ~ protocol ~ status ~ bytes ~ referrer ~ ua =>
+        (ip, time, req, endpoint, protocol, status, bytes, referrer, ua)
+    }
+
   val props = new Properties()
   props.put("bootstrap.servers", "localhost:9092")
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-  // Create the Kafka producer
   val producer = new KafkaProducer[String, String](props)
-
-  // Set up JSON serialization
+  val filename = "logfiles.log"
+  val topicName = "logs"
   implicit val formats = DefaultFormats.withHints(NoTypeHints)
-
   // Read the log file and send each line to Kafka after parsing it as JSON
   val logFile = "src/main/resources/logfiles.log"
-  val source = Source.fromFile(logFile)
-  for (line <- source.getLines) {
-    parse(logEntry, line) match {
-      case Success((ipAddress, timestamp, requestType), _) =>
-        val json = Serialization.write(Map("ip" -> ipAddress, "timestamp" -> timestamp, "requestType" -> requestType))
-        val record = new ProducerRecord[String, String]("logs", null, json)
-        producer.send(record)
-      case _ =>
-        println(s"Failed to parse log line")
-      // Ignore lines that don't match the log format
-    }
-  }
+  Source.fromFile(logFile).getLines().foreach(line => {
+    // Parse the log entry
+    val result = parse(logEntry, line)
+    result match {
+      case Success((ip, time, req, endpoint, protocol, status, bytes, referrer, ua), _) =>
+        // Construct a JSON object from the log entry fields
 
-  source.close()
+        val json = Serialization.write(
+          Map(
+            "ip" -> ip,
+            "timestamp" -> time,
+            "requestType" -> req,
+            "endpoint" -> endpoint,
+            "protocol" -> protocol,
+            "status" -> status.toInt,
+            "bytes" -> bytes.toInt,
+            "referrer" -> referrer,
+            "userAgent" -> ua
+          )
+        )
+        // Send the JSON object as a Kafka message
+        val message = new ProducerRecord[String, String](topicName, json)
+        producer.send(message)
+      case Failure(msg, _) => println(s"Failed to parse log entry: $msg")
+      case Error(msg, _) => println(s"Error while parsing log entry: $msg")
+    }
+  })
   producer.close()
+
 }
