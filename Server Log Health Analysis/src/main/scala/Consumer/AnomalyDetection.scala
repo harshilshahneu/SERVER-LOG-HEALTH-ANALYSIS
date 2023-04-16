c487a9aaca4e9d8671ef13b613b734a00dcbee4b
hrishikesh-pawar <114782182+hrishikesh-pawar@users.noreply.github.com>
2023-04-16 18:04:42 -0400

    Consumer function

--- Server Log Health Analysis/src/main/scala/Consumer/AnomalyDetection.scala
+++ Server Log Health Analysis/src/main/scala/Consumer/AnomalyDetection.scala
@@ -1,41 +1,70 @@
 package Consumer
 
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.classification.OneVsRest
+//import org.apache.spark.ml.classification.OneClassSVM
+import org.apache.spark.ml.classification.OneVsRestModel
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object AnomalyDetection {
 
   def main(args: Array[String]): Unit = {
 
-    // Kafka configuration
+    val conf = new SparkConf().setAppName("AnomalyDetection").setMaster("local[*]")
+    val ssc = new StreamingContext(conf, Seconds(5))
+
     val kafkaParams = Map[String, Object](
       "bootstrap.servers" -> "localhost:9092",
-      "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
-      "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
-      "group.id" -> "anomalyDetectionGroup",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "test-consumer-group",
       "auto.offset.reset" -> "latest",
       "enable.auto.commit" -> (false: java.lang.Boolean)
     )
 
-    // Spark configuration
-    val sparkConf = new SparkConf()
-      .setAppName("AnomalyDetection")
-      .setMaster("local[*]")
-    val ssc = new StreamingContext(sparkConf, Seconds(5))
-
     val topics = Array("logs")
+
     val stream = KafkaUtils.createDirectStream[String, String](
       ssc,
       LocationStrategies.PreferConsistent,
       ConsumerStrategies.Subscribe[String, String](topics, kafkaParams)
     )
 
-    // Create a vector assembler to combine response time and time stamp into a feature vector
-    val assembler = new VectorAssembler()
-      .setInputCols(Array("response_time", "timestamp"))
-      .setOutputCol("features")
-  }
+    //stream.map(record => record.value).print()
 
+    // Process each batch of data from Kafka
+    stream.foreachRDD { rdd =>
+      if (!rdd.isEmpty()) {
+        // Print each record to console
+        rdd.foreach(record => println(record.value()))
+      }
+    }
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
 }
+
+//    // Create a vector assembler to combine response time and time stamp into a feature vector
+//    val assembler = new VectorAssembler()
+//      .setInputCols(Array("response_time", "timestamp"))
+//      .setOutputCol("features")
+//  }
+//
+//  // Define the OCSVM model
+//  val ocsModel = new OneVsRest()
+//    .setClassifier(new OneClassSVM())
+//    .setFeaturesCol("features")
+//    .setPredictionCol("prediction")
+//
+//  // Train the OCSVM model on an empty DataFrame
+//  val emptyDataFrame = Seq.empty[(Double, Double)].toDF("response_time", "timestamp")
+//  val emptyFeatureVector = assembler.transform(emptyDataFrame)
+//  val ocsvmModel = ocsModel.fit(emptyFeatureVector)
+
+
