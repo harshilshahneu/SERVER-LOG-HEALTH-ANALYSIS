4a7927e378b64c4071d0a9f747bca9f7754b4939
Harshil Shah <shah.harshil2@northeastern.edu>
2023-04-18 17:50:51 -0400

    set the log level to error and batching data

--- Server Log Health Analysis/src/main/scala/Consumer/StreamProcessing.scala
+++ Server Log Health Analysis/src/main/scala/Consumer/StreamProcessing.scala
@@ -1,17 +1,23 @@
 package Consumer
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
-import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
 
 case class KafkaMessage(ipAddress: String, dateTime: String, request: String, endpoint: String, protocol: String, status: Int, bytes: Int, referrer: String, userAgent: String, responseTime: Int) extends Serializable
 
 object StreamProcessing {
   def main(args: Array[String]): Unit = {
 
+
+    // Set the logger level to error
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    Logger.getLogger("akka").setLevel(Level.ERROR)
     // Create a Spark configuration
     val conf = new SparkConf()
       .setAppName("StreamingApp")
@@ -39,15 +45,26 @@ object StreamProcessing {
       LocationStrategies.PreferConsistent,
       ConsumerStrategies.Subscribe[String, String](topics, kafkaParams)
     )
-    // Process the Kafka stream
-    kafkaStream.foreachRDD { rdd =>
-      rdd.foreach { record =>
-        val jsonString = record.value()
 
-        implicit val formats = DefaultFormats
-        val kafkaMessage = parse(jsonString).extract[KafkaMessage]
-        println(kafkaMessage)
-        
+    kafkaStream.foreachRDD { rdd =>
+      if (!rdd.isEmpty) {
+        val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
+        import spark.implicits._
+        val df = rdd.map(record => {
+          val jsonString = record.value()
+          implicit val formats = DefaultFormats
+          val kafkaMessage = parse(jsonString).extract[KafkaMessage]
+          kafkaMessage
+        }).toDF()
+
+        // Batch the data into a DataFrame of 500 rows
+        val batchedDF = df.limit(500)
+
+        // Rule-based classification on the data
+        val classifiedDF = batchedDF.filter($"bytes" > 100)
+
+        // Print the classified data to the console
+        classifiedDF.show()
       }
     }
 
